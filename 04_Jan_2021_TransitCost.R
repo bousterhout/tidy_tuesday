f33dc0b89ab76bb9b995ad38afb5c2db9f4e754c
bousterhout <bousterhout@gmail.com>
2021-01-05 20:23:38 -0600

    Pre cleaning code

--- 04_Jan_2021_TransitCost.R
+++ 04_Jan_2021_TransitCost.R
@@ -7,11 +7,12 @@
 ######################
 
 #Objective of project is to determine how to decrease costs of mass transit projects in 
+
 #Libraries
 require(dplyr)
 require(ggplot2)
-require(rworldmap)
-require(passport)
+require(countrycode)
+require(RColorBrewer)
 
 #Read in data
 df <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-01-05/transit_cost.csv')
@@ -37,16 +38,17 @@ hist(mult_project_cities$n)
 
 #Do costs increase or decrease with time? Modestly ($23,000 / year)
 hist(df$cost_km_millions)
-year_mod <- lm(log(cost_km_millions) ~ as.numeric(start_year), data = df)
+year_mod <- lm(log(cost_km_millions) ~ as.numeric(year), data = df)
 plot(year_mod)
 summary(year_mod)
 ggplot(data = df) +
-  geom_jitter(aes(x = cost_km_millions, y = start_year))
+  geom_jitter(aes(x = cost_km_millions, y = year))
 
 
 #Cost by country
 cost_by_country <- df %>% 
   group_by(country) %>% 
+  filter(n() > 5) %>% 
   summarize(mean_cost_per_km = mean(cost_km_millions, na.rm = TRUE),
             median_cost_per_km = median(cost_km_millions, na.rm = TRUE),
             iqr_cost_per_km = IQR(cost_km_millions, na.rm = TRUE),
@@ -68,11 +70,13 @@ cost_mapped <- full_join(base_map_data,
 
 base_map <- ggplot(data = base_map_data, mapping = aes(x = long, y = lat, group = group)) +
   coord_fixed(1.3) + 
-  geom_polygon(color = "white", fill = "gray")
+  geom_polygon() +
+  theme_light()
 
-base_map + geom_polygon(data = cost_mapped, aes(fill = median_cost_per_km), color = "white") +
-  geom_polygon(color = "black", fill = NA) +
-  theme_bw() +
+base_map + geom_polygon(data = cost_mapped, aes(fill = median_cost_per_km)) +
+  geom_polygon(fill = NA) +
+  scale_fill_distiller(name = "Cost per km (millions USD)", palette = 'Spectral')+
+  theme_light() +
   theme(
     axis.text = element_blank(),
     axis.line = element_blank(),
@@ -82,7 +86,68 @@ base_map + geom_polygon(data = cost_mapped, aes(fill = median_cost_per_km), colo
     axis.title = element_blank()
   )
 
-#Cost doesn't appear to be driv
+#Cost by continent
+df$continent <- countrycode(sourcevar = df$country,
+                            origin = "iso2c",
+                            destination = "continent")
+df$continent <- ifelse(df$country == 'US' | df$country == 'CA' | df$country == 'MX',
+                       'N. America',
+                       df$continent)
+df$continent <- ifelse(df$continent == 'Americas', 'S. America', df$continent)
+
+cost_by_continent <- df %>% 
+  group_by(continent) %>% 
+  filter(n() > 10) %>% 
+  mutate(mean_cost_per_km = mean(cost_km_millions, na.rm = TRUE),
+            median_cost_per_km = median(cost_km_millions, na.rm = TRUE),
+            iqr_cost_per_km = IQR(cost_km_millions, na.rm = TRUE),
+            mean_tunnel_per = mean(tunnel / length, na.rm = TRUE),
+            mean_stations_per = mean(stations/length, na.rm = TRUE)) %>% 
+  ungroup()
+
+cost_by_continent$country_name <- as_country_name(cost_by_continent$country,
+                                                short = TRUE,
+                                                variant = TRUE)
+
+cost_by_continent$country_name <- ifelse(cost_by_continent$country == "UK", "UK",
+                                       ifelse(cost_by_continent$country == "US", "USA", cost_by_continent$country_name))
+
+base_map_data <- map_data("world")
+
+cost_mapped <- full_join(base_map_data,
+                         cost_by_continent,
+                         by = c("region" = "country_name"))
+
+base_map <- ggplot(data = base_map_data, mapping = aes(x = long, y = lat, group = group)) +
+  coord_fixed(1.3) + 
+  geom_polygon() +
+  theme_light()
+
+base_map + geom_polygon(data = cost_mapped, aes(fill = median_cost_per_km)) +
+  geom_polygon(fill = NA) +
+  scale_fill_distiller(palette = 'Reds',
+                       direction = 2,
+                       na.value = "grey90",
+                       name = "",
+                       guide = guide_colourbar(direction = "horizontal",
+                                               location = "bottom",
+                                               barwidth = 20,
+                                               barheight = 0.5))+
+  ggtitle("Avg cost of Urban Rail by continent \n (millions USD per km)") +
+  theme_light() +
+  theme(
+    axis.text = element_blank(),
+    axis.line = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_blank(),
+    panel.grid = element_blank(),
+    axis.title = element_blank(),
+    legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5)  
+    )
+
+
+#Cost doesn't appear to be driven by stations of number of tunnels
 base_map + geom_polygon(data = cost_mapped, aes(fill = mean_stations_per), color = "white") +
   geom_polygon(color = "black", fill = NA) +
   theme_bw() +
